10dbd48cb66dbd36d3ed459472eab04f277e392c
2023-1252 <2023-1252@uteco.edu.do>
2026-04-22 20:51:19 -0400

    creacion de nuestra app

---
+++ Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace LP3.Blazorserver.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error { get; set; }
+}
---
+++ Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace LP3.Blazorserver.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
